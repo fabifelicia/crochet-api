c3408a7aa35bb34f2a590ded678edbcfea354dd5
fabifelicia <fabianabarretomenezes@gmail.com>
2025-07-28 21:51:20 -0300

    refactor: ajusta repository para usar dados do banco

--- src/repositories/productRepository.ts
+++ src/repositories/productRepository.ts
@@ -1,54 +1,84 @@
- 
 import Product from "../models/Product";
-import { data } from "../samples/data"
+import { Op } from "sequelize";
 
-const products: Product[] = data;
-
-async function getProductById(id: number | null) : Promise<Product | undefined> {
-    return new Promise((resolve, reject) => {
-        return resolve(products.find(product => product.id === id))
-    })
+async function getProductById(id: number | null): Promise<Product | null> {
+  if (id == null) {
+    return null;
+  }
+  return await Product.findByPk(id);
 }
 
-async function getProductsByTex(tex: number | null) : Promise<Product[]> {
-    return new Promise((resolve, reject) => {
-        return resolve(products.filter(product => product.tex === tex).sort((a : any, b : any) => a - b))
-    })
+async function getProductsByTex(tex: number | null): Promise<Product[]> {
+  if (tex == null) {
+    return [];
+  }
+
+  return await Product.findAll({
+    where: {
+      tex,
+    },
+    order: [["id", "ASC"]],
+  });
 }
 
-async function getProductsByTexRange(texStart: number | null, texEnd: number | null) : Promise<Product[]> {
-    return new Promise((resolve, reject) => {
-        if (texStart == null || texEnd == null) {
-            reject
-        } else {
-            return resolve(products.filter(product => product.tex >= texStart && product.tex <= texEnd).sort((a : any, b : any) => a - b))
-        }
-    })
+async function getProductsByTexRange(
+  texStart: number | null,
+  texEnd: number | null
+): Promise<Product[]> {
+  if (texStart == null || texEnd == null) {
+    return [];
+  }
+
+  return await Product.findAll({
+    where: {
+      tex: {
+        [Op.between]: [texStart, texEnd],
+      },
+    },
+    order: [["id", "ASC"]],
+  });
 }
 
-async function getProductsByName(name:string) : Promise<Product[]> {
-    return new Promise((resolve, reject) => {
-        return resolve(products.filter(product => product.name.toLowerCase() === name.toLowerCase()).sort((a : any, b : any) => a - b))
-    })
+async function getProductsByName(name: string): Promise<Product[]> {
+  if (!name) {
+    return [];
+  }
+
+  return await Product.findAll({
+    where: {
+      name: {
+        [Op.iLike]: name,
+      },
+    },
+    order: [["id", "ASC"]],
+  });
 }
 
-async function getProductsByBrand(brand:string) : Promise<Product[]> {
-    return new Promise((resolve, reject) => {
-        return resolve(products.filter(product => product.brand.toLowerCase() === brand.toLowerCase()).sort((a : any, b : any) => a - b))
-    })
+
+async function getProductsByBrand(brand: string): Promise<Product[]> {
+  if (!brand) {
+    return [];
+  }
+
+  return await Product.findAll({
+    where: {
+      brand: {
+        [Op.iLike]: brand,
+      },
+    },
+    order: [["id", "ASC"]],
+  });
 }
 
-async function getAllProducts() : Promise<Product[]> {
-    return new Promise((resolve, reject) => {
-        return resolve(products);
-    })
+async function getAllProducts(): Promise<Product[]> {
+  return await Product.findAll();
 }
 
 export default {
-    getProductsByTex,
-    getProductsByTexRange,
-    getProductsByName,
-    getProductsByBrand,
-    getProductById,
-    getAllProducts
-}
+  getProductsByTex,
+  getProductsByTexRange,
+  getProductsByName,
+  getProductsByBrand,
+  getProductById,
+  getAllProducts,
+};
