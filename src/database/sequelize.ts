d22d05673515208a797e75de8e0958167e6191c4
fabifelicia <fabianabarretomenezes@gmail.com>
2025-08-02 17:36:12 -0300

    fix : adjust sequelize by tests

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -4,21 +4,24 @@ import { env } from '../config/env';
 const connectionString =
   env.NODE_ENV === 'production' ? env.DATABASE_URL! : env.DATABASE_PUBLIC_URL!;
 
-if (!connectionString) {
-  throw new Error('DATABASE_URL não definida');
+if (env.NODE_ENV !== 'test' && !connectionString) {
+  throw new Error('DATABASE_URL not defined');
 }
 
-const sequelize = new Sequelize(connectionString, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-    keepAlive: true,
-  },
-});
+const sequelize =
+  env.NODE_ENV === 'test'
+    ? ({} as Sequelize)
+    : new Sequelize(connectionString, {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        logging: false,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+      });
 
 export default sequelize;
