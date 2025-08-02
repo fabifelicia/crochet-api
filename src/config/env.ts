df00ffebe2bbba305f0eb331abdc8f6f8c36872e
fabifelicia <fabianabarretomenezes@gmail.com>
2025-08-02 17:29:56 -0300

    fix: env.ts by tests

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,15 +4,17 @@ dotenv.config();
 
 const requiredEnv = ['PORT', 'DATABASE_URL', 'DATABASE_PUBLIC_URL', 'NODE_ENV'];
 
-for (const key of requiredEnv) {
-  if (!process.env[key]) {
-    console.error(`A variável de ambiente ${key} não foi definida`);
-    process.exit(1);
+if (process.env.NODE_ENV !== 'test') {
+  for (const key of requiredEnv) {
+    if (!process.env[key]) {
+      console.error(`Environment variable ${key} not defined`);
+      process.exit(1);
+    }
   }
 }
 
 export const env = {
-  PORT: process.env.PORT as string,
+  PORT: process.env.PORT as string | 3000,
   DATABASE_URL: process.env.DATABASE_URL as string,
   DATABASE_PUBLIC_URL: process.env.DATABASE_PUBLIC_URL as string,
   NODE_ENV: process.env.NODE_ENV as string,
