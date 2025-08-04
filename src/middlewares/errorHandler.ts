f6a42cc28143ba9ab079256b6b24b27d9d2bbc1c
fabifelicia <fabianabarretomenezes@gmail.com>
2025-08-04 18:04:57 -0300

    chore : adjust lint

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response, NextFunction } from 'express';
 import logger from '../config/logger';
